b47cfe31e1009e6d8d38f8a31aeaee159de1c308
knakayama <knakayama.sh@gmail.com>
2017-05-06 17:42:18 +0900

    test

--- helpers/endpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Assuming you deployed with a 'serverless deploy | tee deploy.out' this script
-# will auto fill in the TODOS_ENDPOINT environment variable
-
-node helpers/getEndpoint.js > .build/endpoint.out
-echo $TODOS_ENDPOINT
-export TODOS_ENDPOINT="$(cat .build/endpoint.out)"
-
